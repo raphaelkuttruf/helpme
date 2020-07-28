947b514d9c58c1d2ef8fb747bfa0bd6e6676bdb9
Raphael Kuttruf <Raphael.Kuttruf@gmx.net>
2020-07-29 00:02:19 +0200

    Fix check, if VMWare Tools package is mounted

--- vmware/InstallVMWareTools.sh
+++ vmware/InstallVMWareTools.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-if [[ -f /media/$USER/VMware\ Tools/VMwareTools*.tar.gz ]]
+# Search for mounted VMWare Tools package
+files=$(ls /media/$USER/VMware\ Tools/VMwareTools*.tar.gz 2> /dev/null | wc -l)
+if [ "$files" == "1" ] # one Package found
 then
-# Extract mounted VMWare Tools  to temporary directory
-tar xzvf /media/$USER/VMware\ Tools/VMwareTools*.tar.gz -C /tmp/
-echo "Installing VMWare Tools."
-cd /tmp/vmware-tools-distrib
-# Install VMWare Tools with default settings
-sudo ./vmware-install.pl -d
+    # Extract mounted VMWare Tools to temporary directory
+    tar xzvf /media/$USER/VMware\ Tools/VMwareTools*.tar.gz -C /tmp/
+    echo "Installing VMWare Tools."
+    cd /tmp/vmware-tools-distrib
+    # Install VMWare Tools with default settings
+    sudo ./vmware-install.pl -d
 else
-echo "VMWare Tools are not mounted..."
+    echo "VMWare Tools are not mounted @ /media/$USER/VMware\ Tools/..."
 fi
